83015715f0dd42b43d7078844a0678f5eea6490f
Enzo Lima <olucca104@gmail.com>
2026-04-05 00:26:47 -0300

    Adiciona ProductController.

---
+++ src/main/java/one/digitalinnovation/patterns/controller/ProductController.java
@@ -0,0 +1,43 @@
+package one.digitalinnovation.patterns.controller;
+
+import lombok.RequiredArgsConstructor;
+import one.digitalinnovation.patterns.domain.Product;
+import one.digitalinnovation.patterns.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.findAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.findProductById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
